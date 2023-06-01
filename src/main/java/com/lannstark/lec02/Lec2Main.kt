78111029610fec8a47e636f13d153341b068e712
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-06-01 22:20:07 +0900

    safe call

--- src/main/java/com/lannstark/lec02/Lec2Main.kt
+++ src/main/java/com/lannstark/lec02/Lec2Main.kt
@@ -3,12 +3,29 @@ package com.lannstark.lec02
 import java.lang.IllegalArgumentException
 
 fun main(){
+    val str: String? = null
+    println(str?.length)
+
     
 }
 
 fun startsWithA1(str: String?): Boolean {
     if (str == null) {
         throw IllegalArgumentException("null 들어옴")
+    }
+    return str.startsWith("A")
+}
+
+fun startsWithA2(str: String?): Boolean? {
+    if(str == null) {
+        return null
+    }
+    return str.startsWith("A")
+}
+
+fun startsWithA3(str: String?): Boolean {
+    if (str == null) {
+        return false
     }
     return str.startsWith("A")
 }
