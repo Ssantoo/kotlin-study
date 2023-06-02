0a3a99ba5e9680e4155f4fe45e320a222b9ea0cc
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-06-02 20:13:24 +0900

    null 처리

--- src/main/java/com/lannstark/lec02/Lec2Main.kt
+++ src/main/java/com/lannstark/lec02/Lec2Main.kt
@@ -5,8 +5,13 @@ import java.lang.IllegalArgumentException
 fun main(){
     val str: String? = null
     println(str?.length)
+    //null
 
-    
+    /////
+
+    println(str?.length ?: 0)
+    //0
+
 }
 
 fun startsWithA1(str: String?): Boolean {
@@ -15,6 +20,12 @@ fun startsWithA1(str: String?): Boolean {
     }
     return str.startsWith("A")
 }
+//좀더 코틀린스럽게
+fun startsWithA1_1(str: String?): Boolean {
+    return str?.startsWith("A") ?: throw IllegalArgumentException("null 들어옴")
+}
+
+
 
 fun startsWithA2(str: String?): Boolean? {
     if(str == null) {
@@ -22,10 +33,26 @@ fun startsWithA2(str: String?): Boolean? {
     }
     return str.startsWith("A")
 }
+//좀더 코틀린 스럽게
+fun startsWithA2_1(str: String?): Boolean? {
+    return str?.startsWith("A")
+}
+
+
 
 fun startsWithA3(str: String?): Boolean {
     if (str == null) {
         return false
     }
     return str.startsWith("A")
+}
+//좀더 코틀린 스럽게
+fun startsWithA3_1(str: String?): Boolean {
+    return str?.startsWith("A") ?: false
+}
+
+
+///// null이 무조건 아닐때
+fun startsWith(str: String?): Boolean {
+    return str!!.startsWith("A")
 }
