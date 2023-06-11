51ad0086bb916a99da1f913fd470e473d215faa5
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-06-11 18:31:17 +0900

    코틀린 비교2

--- src/main/java/com/lannstark/lec04/lec4main.kt
+++ src/main/java/com/lannstark/lec04/lec4main.kt
@@ -2,9 +2,29 @@ package com.lannstark.lec04
 
 fun main(){
     val money1 = JavaMoney(2_000L)
-    val money2 = JavaMoney(1_000L)
+    val money2 = money1
+    val money3 = JavaMoney(1_000L)
 
     if (money1 > money2){
         println("money1가 비쌈")
     }
+
+    println(money1 === money3)
+
+
+    if(fun1() || fun2()){
+        println("본문")
+    }
+
+
+}
+
+fun fun1(): Boolean {
+    println("fun 1")
+    return true
+}
+
+fun fun2(): Boolean {
+    println("fun2")
+    return false
 }
